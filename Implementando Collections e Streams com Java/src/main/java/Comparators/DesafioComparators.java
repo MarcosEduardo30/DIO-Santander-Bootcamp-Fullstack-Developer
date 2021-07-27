8c8cf7b8de961679a1aafb8260534530a553fd38
MarcosEduardo30 <marcos302k02@gmail.com>
2021-07-26 22:25:56 -0300

    Finalizado o desafio da aula 5 de Collections e Streams

--- Implementando Collections e Streams com Java/src/main/java/Comparators/DesafioComparators.java
+++ Implementando Collections e Streams com Java/src/main/java/Comparators/DesafioComparators.java
@@ -37,5 +37,16 @@ public class DesafioComparators {
             System.out.println(conta);
         }
 
+        //Ordenando um treeMap usando uma função lambda como parâmetro
+        TreeMap<ContaBanco, String> treeContas = new TreeMap<>((ContaBanco conta1, ContaBanco conta2) ->
+                conta1.getNomeCliente().compareTo(conta2.getNomeCliente()));
+        treeContas.put(new ContaBanco("1234", "Marcelo", true), "Conta 1");
+        treeContas.put(new ContaBanco("4321", "Arnaldo", true), "Conta 2");
+        treeContas.put(new ContaBanco("9876", "Amanda", false), "Conta 3");
+        treeContas.put(new ContaBanco("6789", "Larissa", true), "Conta 4");
+        treeContas.put(new ContaBanco("3691", "Joaquim", false), "Conta 5");
+        for (Map.Entry<ContaBanco, String> conta : treeContas.entrySet()){
+            System.out.println("-->" + conta);
+        }
     }
 }
